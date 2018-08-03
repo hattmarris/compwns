53704ae43794c6abe2c4d07d8c34a33b2a5ffef9
Matt Harris <mattharris89@gmail.com>
2018-08-03 18:24:55 -0400

    Adds babel-plugin-styled-components to transform styled. syntax into displayName for components for easier (more standard) shallow enzyme testing - adds a test for showing the drop when main select is clicked and ignores test files in build step

---
+++ components/Dropdown.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Dropdown from "./Dropdown";
+import faker from "faker";
+import { times } from "lodash";
+
+describe("Rendering tests for Dropdown", () => {
+    const options = times(10, () => faker.lorem.word());
+    const dropdown = shallow(<Dropdown {...{ options } } />);
+    
+    test("Dropdown renders with options", () => {
+      expect(dropdown.exists()).toBe(true);
+    });
+  
+    test("When Select is clicked, Drop is shown", () => {
+      const Select = dropdown.find("[role='select']");
+      
+      Select.simulate("click");
+      
+      expect(dropdown.find("Drop").exists()).toBe(true);  
+    });  
+
+}); 
